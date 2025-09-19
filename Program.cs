481c5e7dc7467f7414de1e36410eece326d501eb
Emma Karlholm <emma.karlholm@gmail.com>
2025-09-19 18:20:14 +0200

    Title screen added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("\t\tWelcome to");
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("\t\t\tNumb3rs G4m3");
+            Console.ResetColor();
+            Console.WriteLine("\t\t\t\tby Emma Karlholm");
+                
         }
     }
 }
+
+
